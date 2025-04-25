49dc66fc29a868a37278d9ed4b133a8936d7cffc
AthiSoft <teamvenu058@gmail.com>
2025-04-25 15:06:04 +0530

    Scrollable effects and some tweaks

---
+++ assets/custom.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+const section = document.getElementById("local-packages-section");
+
+if(window.innerWidth <= 768) {
+section.setAttribute("data-aos","fade-up");
+} else {
+section.setAttribute("data-aos","fade-left");
+}
+
+});
